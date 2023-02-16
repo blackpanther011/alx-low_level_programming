f428a1ea9f37d1fee7041910732ab467f8646b31
okaforkingsley <okaforkingsley2004@gmail.com>
2023-02-16 15:16:29 -0800

    its getting intresting now

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- A program that uses puts to print a string
+ *Return: ends with 0
+ */
+int main()
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
+
