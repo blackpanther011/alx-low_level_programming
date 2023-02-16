1b170c5a0fc438e1f9c7d57d0c3093096b79cf4b
okaforkingsley <okaforkingsley2004@gmail.com>
2023-02-16 15:19:55 -0800

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  *main- A program that uses puts to print a string
  *Return: ends with 0
  */
-int main()
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
