78d282b8dbcd0e654aa080ece348e4a067de78c2
okaforkingsley <okaforkingsley2004@gmail.com>
2023-02-16 16:14:53 -0800

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**                                                   
 * main - prints a string and standard error
- * Return: 1                                          
+* Return:1                                          
 */
 int main(void)                                       
 {	               
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2,"and that piece of art is useful\" - Dora Korpar,2015-10-19\n",59);
 return (1);
 }
